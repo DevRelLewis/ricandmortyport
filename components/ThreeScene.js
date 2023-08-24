483ad09acdca503fa38e4fdb05131657ee62c7c7
Lewis Meyers <meyerslewis193@gmail.com>
2023-08-23 21:55:26 -0400

    final 3d model and animations

--- components/ThreeScene.js
+++ components/ThreeScene.js
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from "react";
 import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader.js";
 
 const ThreeScene = () => {
   const sceneRef = useRef(null);
@@ -19,97 +20,101 @@ const ThreeScene = () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
     sceneRef.current.appendChild(renderer.domElement);
 
-    // Add ambient light to the scene
-    const ambientLight = new THREE.AmbientLight(0xffffff, 4);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 6);
     scene.add(ambientLight);
 
-    // Load the GLB model (first model)
     const loader = new GLTFLoader();
     loader.load("/rick.glb", (gltf) => {
       const model = gltf.scene;
       scene.add(model);
 
-      // Calculate center and bounding box of the model
       const boundingBox = new THREE.Box3().setFromObject(model);
       const center = boundingBox.getCenter(new THREE.Vector3());
 
-      // Set camera position and look at the center of the model
       camera.position.set(0.1, 1.79, 3);
       camera.lookAt(center);
 
-      // Add camera controls for user interaction
       const controls = new OrbitControls(camera, renderer.domElement);
       controls.target.copy(center);
       controls.enableZoom = false;
       controls.enablePan = true;
       controls.update();
 
-      // Load the GLB model (second model)
       loader.load("/clipboard.glb", (secondGltf) => {
         const secondModel = secondGltf.scene;
-
-        // Position the second model within the interior of the first model
-        secondModel.position.set(0.5, 1, 4.5); // Adjust x, y, z values
-
-        // Add the second model as a child of the first model
+        secondModel.position.set(0.5, 1, 4.5);
         model.add(secondModel);
       });
 
-      // Load the GLB model (3rd model)
       loader.load("/retro_computer.glb", (thirdGltf) => {
         const thirdModel = thirdGltf.scene;
-
-        // Position the 3rd model within the interior of the first model
-        thirdModel.position.set(-2.9, 1.33, 3.6); // Adjust x, y, z values
-
-        // Set the scale of the third model
-        thirdModel.scale.set(0.35, 0.35, 0.35); // Adjust x, y, z scale values
-
-        // Add the third model as a child of the first model
+        thirdModel.position.set(-2.9, 1.33, 3.6);
+        thirdModel.scale.set(0.35, 0.35, 0.35);
         model.add(thirdModel);
       });
 
-      // Load the GLB model (4th model)
       loader.load("/pickle_jar.glb", (fourthGltf) => {
         const fourthModel = fourthGltf.scene;
-
-        // Position the fourth model within the interior of the first model
-        fourthModel.position.set(0.5, 1, 5.3); // Adjust x, y, z values
-
-        // Set the scale of the fourth model
-        fourthModel.scale.set(0.005, 0.005, 0.005); // Adjust x, y, z scale values
-
-        // Add the fourth model as a child of the first model
+        fourthModel.position.set(0.5, 1, 5.3);
+        fourthModel.scale.set(0.005, 0.005, 0.005);
         model.add(fourthModel);
       });
 
-      // Load the GLB model (5th model)
       loader.load("/portal_gun.glb", (fifthGltf) => {
         const fifthModel = fifthGltf.scene;
-
-        // Position the fifth model within the interior of the first model
-        fifthModel.position.set(-0.02, 1.1, 5.3); // Adjust x, y, z values
-
-        // Set the scale of the fifth model
-        fifthModel.scale.set(2, 2, 2); // Adjust x, y, z scale values
-
-        // Rotate the fifth model on the x-axis (in degrees)
-        fifthModel.rotation.y = THREE.MathUtils.degToRad(180); // Rotate 45 degrees on x-axis
-
-        // Add the fifth model as a child of the first model
+        fifthModel.position.set(-0.02, 1.1, 5.3);
+        fifthModel.scale.set(2, 2, 2);
+        fifthModel.rotation.y = THREE.MathUtils.degToRad(180);
         model.add(fifthModel);
       });
 
-      // Start rendering loop
+      let mixer;
+      const animations = [
+        "rickAnim1.fbx",
+        "rickAnim2.fbx",
+        "rickAnim3.fbx",
+        "rickAnim4.fbx",
+        "rickAnim5.fbx",
+      ];
+      const fbxLoader = new FBXLoader();
+      let loadedAnimations = 0; // Counter for loaded animations
+
+      fbxLoader.load("rick_3d.fbx", (object) => {
+        animations.forEach((anim) => {
+          fbxLoader.load(anim, (animObject) => {
+            if (animObject.animations) {
+              animObject.animations.forEach((animation) => {
+                object.animations.push(animation);
+              });
+            }
+            loadedAnimations++;
+
+            if (
+              loadedAnimations === animations.length &&
+              object.animations[3]
+            ) {
+              mixer = new THREE.AnimationMixer(object);
+              const idleAction = mixer.clipAction(object.animations[4]);
+              idleAction.play();
+            }
+          });
+        });
+
+        object.position.set(-1.5, -0.01, 5.1);
+        object.scale.set(0.007, 0.007, 0.007);
+        object.rotation.y = THREE.MathUtils.degToRad(150);
+        model.add(object);
+      });
+
       const animate = () => {
         requestAnimationFrame(animate);
-        controls.update(); // Update controls in the animation loop
+        if (mixer) mixer.update(0.01); // Update the animation mixer
+        controls.update();
         renderer.render(scene, camera);
       };
       animate();
     });
 
-    // Handle window resizing
     const handleResize = () => {
       const newWidth = window.innerWidth;
       const newHeight = window.innerHeight;
@@ -118,8 +123,6 @@ const ThreeScene = () => {
       renderer.setSize(newWidth, newHeight);
     };
     window.addEventListener("resize", handleResize);
-
-    // Text handling
 
     return () => {
       window.removeEventListener("resize", handleResize);
