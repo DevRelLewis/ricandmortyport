9510506297ff609e2c26ba687f59db1459cbdd1e
Lewis Meyers <meyerslewis193@gmail.com>
2023-09-11 20:11:17 -0400

    theme song added

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 // pages/index.js
 "use client";
 import React, { useState, useEffect } from "react";
+import { Howl, Howler } from "howler";
 import Head from "next/head";
 import ThreeScene from "../components/ThreeScene";
 import Modal from "../components/Modal";
@@ -8,6 +9,25 @@ import Modal from "../components/Modal";
 const HomePage = () => {
   const [modalIndex, setModalIndex] = useState(0);
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    // Initialize Howler
+    const themeMusic = new Howl({
+      src: ["/themesong.mp3"], // Replace with the actual path to your theme music
+      loop: true, // Loop the music continuously
+      volume: 0.5, // Adjust the volume as needed
+    });
+
+    // Play the theme music when the component mounts
+    themeMusic.play();
+
+    // Cleanup function
+    return () => {
+      // Stop and unload the theme music when the component unmounts
+      themeMusic.stop();
+      themeMusic.unload();
+    };
+  }, []);
 
   useEffect(() => {
     if (modalIndex < 4) {
